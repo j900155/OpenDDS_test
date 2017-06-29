09336fa6f3f0310585a021af79895df18a4bd26a
j900155 <j9001552@gmail.com>
2017-06-29 20:05:20 +0800

    add input_cmd

---
+++ input_cmd/Publisher.cpp
@@ -0,0 +1,172 @@
+/*
+ *
+ *
+ * Distributed under the OpenDDS License.
+ * See: http://www.opendds.org/license.html
+ */
+
+#include <ace/Log_Msg.h>
+
+#include <dds/DdsDcpsInfrastructureC.h>
+#include <dds/DdsDcpsPublicationC.h>
+
+#include <dds/DCPS/Marked_Default_Qos.h>
+#include <dds/DCPS/Service_Participant.h>
+#include <dds/DCPS/WaitSet.h>
+
+#include "dds/DCPS/StaticIncludes.h"
+
+#include "MessengerTypeSupportImpl.h"
+
+//time stamp
+#include <sys/time.h>
+
+#include <iostream>
+int
+ACE_TMAIN(int argc, ACE_TCHAR *argv[])
+{
+  try {
+    // Initialize DomainParticipantFactory
+    DDS::DomainParticipantFactory_var dpf =
+      TheParticipantFactoryWithArgs(argc, argv);
+
+    // Create DomainParticipant
+    DDS::DomainParticipant_var participant =
+      dpf->create_participant(42,
+                              PARTICIPANT_QOS_DEFAULT,
+                              0,
+                              OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+
+    if (!participant) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" create_participant failed!\n")),
+                       -1);
+    }
+
+    // Register TypeSupport (Messenger::Message)
+    Messenger::MessageTypeSupport_var ts =
+      new Messenger::MessageTypeSupportImpl;
+
+    if (ts->register_type(participant, "") != DDS::RETCODE_OK) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" register_type failed!\n")),
+                       -1);
+    }
+
+
+    // Create Topic (Movie Discussion List)
+	std::string topic_name;
+	std::cout << "topic name? ";
+	std::cin >> topic_name;
+	std::cout << "topic name " << topic_name << std::endl;
+    CORBA::String_var type_name = ts->get_type_name();
+    DDS::Topic_var topic =
+      participant->create_topic(topic_name.c_str(),
+                                type_name,
+                                TOPIC_QOS_DEFAULT,
+                                0,
+                                OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+
+    if (!topic) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" create_topic failed!\n")),
+                       -1);
+    }
+
+
+    // Create Publisher
+    DDS::Publisher_var publisher =
+      participant->create_publisher(PUBLISHER_QOS_DEFAULT,
+                                    0,
+                                    OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+
+    if (!publisher) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" create_publisher failed!\n")),
+                       -1);
+    }
+	
+    DDS::DataWriter_var writer =
+      publisher->create_datawriter(topic,
+                                   DATAWRITER_QOS_DEFAULT,
+                                   0,
+                                   OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+
+    if (!writer) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" create_datawriter failed!\n")),
+                       -1);
+    }
+
+    Messenger::MessageDataWriter_var message_writer =
+      Messenger::MessageDataWriter::_narrow(writer);
+
+    if (!message_writer) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" _narrow failed!\n")),
+                       -1);
+    }
+
+    // Block until Subscriber is available
+    DDS::StatusCondition_var condition = writer->get_statuscondition();
+    condition->set_enabled_statuses(DDS::PUBLICATION_MATCHED_STATUS);
+
+    DDS::WaitSet_var ws = new DDS::WaitSet;
+
+	//time
+	struct timeval tv;
+	gettimeofday(&tv,NULL);
+    // Write samples
+	std::string text;
+    Messenger::Message message;
+	while(true)
+	{
+		message.sendData = "";
+		std::cout << "send data"<< std::endl;
+		getline(std::cin, text);
+		if(text == "exit")
+		{
+			std::cout << "exit" << std::endl;
+			break;
+		}
+		message.sendData = text.c_str();
+	    message.sendTime  = 0;
+
+		  gettimeofday(&tv,NULL);
+	      DDS::ReturnCode_t error = message_writer->write(message, DDS::HANDLE_NIL);
+		  message.sendTime = tv.tv_usec;
+	      if (error != DDS::RETCODE_OK)
+		  {
+		    ACE_ERROR((LM_ERROR,
+            ACE_TEXT("ERROR: %N:%l: main() -"),
+            ACE_TEXT(" write returned %d!\n"), error));
+		}
+	}
+    // Wait for samples to be acknowledged
+    DDS::Duration_t timeout = { 30, 0 };
+    if (message_writer->wait_for_acknowledgments(timeout) != DDS::RETCODE_OK) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" wait_for_acknowledgments failed!\n")),
+                       -1);
+    }
+
+    // Clean-up!
+    participant->delete_contained_entities();
+    dpf->delete_participant(participant);
+
+    TheServiceParticipant->shutdown();
+
+  } catch (const CORBA::Exception& e) {
+    e._tao_print_exception("Exception caught in main():");
+    return -1;
+  }
+
+  return 0;
+}
---
+++ input_cmd/Subscriber.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <dds/DdsDcpsInfrastructureC.h>
+#include <dds/DdsDcpsPublicationC.h>
+
+#include <dds/DCPS/Marked_Default_Qos.h>
+#include <dds/DCPS/Service_Participant.h>
+#include <dds/DCPS/WaitSet.h>
+
+#include "dds/DCPS/StaticIncludes.h"
+
+#include "MessengerTypeSupportImpl.h"
+
+
+int ACE_TMAIN(int argc, char *argv[])
+{
+	DDS::DomainParticipantFactory_var dpf = TheParticipantFactoryWithArgs(argc, argv);
+	DDS::DomainParticipant_var participant = dpf-> create_participant(42,
+																	PARTICIPANT_QOS_DEFAULT,
+																	0,
+																	OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+	std::cout << "participant" << participant->get_domain_id() << std::endl;
+
+	Messenger::MessageTypeSupport_var ts = new Messenger::MessageTypeSupportImpl;
+	if (ts->register_type(participant, "") != DDS::RETCODE_OK)
+	{
+		std::cerr <<"create ts fail"<< std::endl;
+	}
+	CORBA::String_var type_name = ts->get_type_name();
+	std::cout << "get_type_name" << type_name << std::endl;
+	//topic
+	
+	std::string topic_name;
+	std::cout << "topic name?" << std::endl;
+	std::cin >> topic_name;
+	std::cout << "topic name " << topic_name << std::endl;
+
+	DDS::TopicQos topic_Qos;
+	participant->get_default_topic_qos(topic_Qos);
+	DDS::Topic_var topic = participant->create_topic(topic_name.c_str(),
+										type_name,
+										topic_Qos,
+										0,
+										OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+	if(!topic)
+	{
+		std::cerr <<"create topic fail"<< std::endl;
+	}
+	//topic end
+	//subscriber
+	DDS::SubscriberQos sub_qos;
+	participant->get_default_subscriber_qos(sub_qos);
+
+	DDS::Subscriber_var subscriber = 
+			participant->create_subscriber(sub_qos,
+				0,
+				OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+	DDS::DataReaderQos readerQos;
+	if(!subscriber)
+	{
+		std::cerr <<"create subcriber fail"<< std::endl;
+	}
+	else
+	{
+		subscriber->get_default_datareader_qos(readerQos);
+	}
+	//subscriber end
+	//reader
+	DDS::DataReader_var reader1 = subscriber->create_datareader(
+			topic,
+	readerQos,
+			0,
+			OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+	if(!reader1)
+	{
+		std::cerr <<"create reader fail"<< std::endl;
+	}
+	//reader end
+	//datareader
+	Messenger::MessageDataReader_var dataReader = Messenger::MessageDataReader::_narrow(reader1);
+	if(!dataReader)
+	{
+		std::cerr <<"create dataReader fail"<< std::endl;
+	}
+
+	std::cout<< "topic name " << topic->get_name() << std::endl;
+
+	DDS::ReturnCode_t error;
+	DDS::SampleInfo info;
+	Messenger::Message message;
+	while(true)
+	{
+		error = dataReader->take_next_sample(message, info);
+		if(error == DDS::RETCODE_OK)
+		{
+			std::cout << "SampleInfo.sample_rank = " << info.sample_rank << std::endl;
+		    std::cout << "SampleInfo.instance_state = " << info.instance_state << std::endl;
+			if(info.valid_data)	
+			{
+				std::cout<< "topic name " << topic->get_name() << std::endl;
+				std::cout << "message data " << message.sendData << std::endl;
+				std::cout << "message time " << message.sendTime << std::endl;
+			}
+		}
+		usleep(10);
+
+	}
+	return 0;
+}
+
